5ef038bdf924b5b520999ef7c33a55806b47628c
umeyuu <91179464+umeyuu@users.noreply.github.com>
2023-02-18 03:01:43 +0900

    ポート番号を可変に変更

--- main.go
+++ main.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
+		// 引数で受け取ったnet.Listenerを利用するので、Addr は指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -19,9 +21,10 @@ func run(ctx context.Context) error {
 	eg, ctx := errgroup.WithContext(ctx)
 	// 別ゴルーチンでHTTPサーバーを起動する
 	eg.Go(func() error {
-		// http.ErrServerClosed は　http.Server.Shutdown() が正常に終了したことを意味する
-		if err := s.ListenAndServe(); err != nil &&
+		// ListenAndServeメソッドではなく、Serveメソッドに変更
+		if err := s.Serve(l); err != nil &&
 			err != http.ErrServerClosed {
+			// http.ErrServerClosed は　http.Server.Shutdown() が正常に終了したことを意味する
 			log.Printf("failed to close: %+v", err)
 			return err
 		}
@@ -38,7 +41,18 @@ func run(ctx context.Context) error {
 }
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Println("need port number")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("faild to listen port %s: %v", p, err)
+	}
+
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
